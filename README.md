600aa13e532b09578a3d62e7811f11edfda8bd41
Yunzhe0329 <zac90613@gmail.com>
2026-09-20 16:44:45 +0800

    FastAPI字串與路徑參數的處理

---
+++ README.md
@@ -0,0 +1,5 @@
+## 字串與參數處理
+- from typing import Annotated : 這個lib可以幫你的前端帶入的參數做型別標註，確保 input 是 back-end 預期的格式
+- Path() : 用來處理路徑參數 (必須包含在路徑當中的參數); Query() : 用來處理URL?後面帶上的參數(選擇性帶上)
+
+---
---
+++ main.py
@@ -0,0 +1,35 @@
+# 幫 input 做資料格式的標註
+from typing import Annotated
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return "Hello FastAPI"
+
+# Query() — 參數是網址問號後面的查詢字串, "GET /hello?name=Tom"
+@app.get("/hello")
+def hello(name:Annotated[str, Query(min_length=2, max_length=30)]):
+    message = f"Hello {name}"
+    return {"message": message}
+
+# Path() — 參數是網址路徑的一部分"/square/{number}"
+@app.get("/square/{number}")
+def square(number: Annotated[int, Path(ge=1)]):
+    result = number * number
+    return {"result" : result}
+
+@app.get("/multiply")
+def multiply(
+    n1: Annotated[int, Query(ge=0, le=10)],
+    n2: Annotated[int, Query(ge=0, le=10)]
+):
+    n1 = int(n1)
+    n2 = int(n2)
+    result = n1 * n2
+    return {"result": result}
+
+@app.get("/echo/{name}")
+def echo(name: Annotated[str, Path(min_length=2, max_length=30, description="")]):
+    return {"message": "Hello" + name}
